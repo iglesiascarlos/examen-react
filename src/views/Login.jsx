1aeb74c0fae4cf2c76fc27eb60613a20745787a6
Carlos Iglesias <iglesiasscarlos@gmail.com>
2023-03-21 18:29:55 +0100

    fin examen 2.0

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -6,36 +6,52 @@ export default function Login() {
 
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
-    const [islogin, setIslogin] = useState(false);
+    const [password, setPassword] = useState('');
 
+    
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+    
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+    
     const onSubmit = (e) => {
         e.preventDefault();
+        const emailIsValid = /\S+@\S+\.\S+/.test(email);
+        const passwordIsValid = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password);
 
-        if (!e.target.email.value) {
-            alert("Email obligatorio");
-        } else if (!e.target.email.value) {
-            alert("Email no valido");
-        } else if (!e.target.password.value) {
-            alert("Contraseña Obligatoria");
-        } else if (e.target.email.value === "examen@ejemplo.com" && e.target.password.value === "examen"){
-            navigate('/')
-            setEmail(e.target.email.value);
-            setIslogin(true)
-            e.target.email.value = "";
-            e.target.password.value = "";
+        if (emailIsValid && passwordIsValid) {
+            if (!e.target.email.value) {
+                alert("Email obligatorio");
+            } else if (!e.target.email.value) {
+                alert("Email no valido");
+            } else if (!e.target.password.value) {
+                alert("Contraseña Obligatoria");
+            } else if (e.target.email.value === "examen@ejemplo.com" && e.target.password.value === "Examen1234"){
+                navigate('/')
+                setEmail(e.target.email.value);
+                e.target.email.value = "";
+                e.target.password.value = "";
+            } else {
+                alert("Email o contraseña incorrectos");
+            }
         } else {
-            alert("Email o contraseña incorrectos");
+            alert('el email o la contraseña no cumplen los requisitos')
         }
     }
+
+
 
     return (
         <div className="login">
             <h1>Form Login</h1>
             <form onSubmit={onSubmit}>
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
+                <input type="email" name="email" id="email" value={email} onChange={handleEmailChange}/>
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
+                <input type="password" name="password" id="password" value={password} onChange={handlePasswordChange}/>
                 <button>LogIn</button>
             </form>
                 <Link to='/'>
